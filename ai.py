aa2bf840f45091e27c706587b878d01289d23fb8
Nathan Rolfes <nathanzrolfes@gmail.com>
2022-03-10 14:51:09 -0500

    AI now randomly picks a spot from the middle piles when placing cards down

--- ai.py
+++ ai.py
@@ -1,4 +1,6 @@
 from player import Player
+import random
+
 
 class AI(Player):
     def __init__(self, playerNum):
@@ -10,9 +12,11 @@ class AI(Player):
         go_again = True
         while go_again:
             go_again = False
+            y = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+            random.shuffle(y)
 
             # go through the blitz pile
-            for x in range(0, 12):
+            for x in y:
                 if len(self.blitzPile) > 0:
                     if self.playAttempt(board, x, self.blitzPile[0]):
                         self.playResultForBlitzPile(True, board, x)
@@ -20,14 +24,14 @@ class AI(Player):
 
             # go through the stack pile
             for index in range(0, 3):
-                for x in range(0, 12):
+                for x in y:
                     if len(self.stackingPiles[index]) > 0:
                         if self.playAttempt(board, x, self.stackingPiles[index][0]):
                             self.playResultForStackPile(True, board, x, index)
                             go_again = True
 
             # go through the flip pile
-            for x in range(0, 12):
+            for x in y:
                 if len(self.placePile) > 0:
                     if self.playAttempt(board, x, self.placePile[0]):
                         self.playResultForPlacePile(True, board, x)
